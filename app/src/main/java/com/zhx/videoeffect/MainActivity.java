c46ea9f2a614000deb704612b905ff8982ad58c8
zhx <31560362+zhxmdefj@users.noreply.github.com>
2020-06-17 21:39:40 +0800

    MediaPlayer播放视频

--- app/src/main/java/com/zhx/videoeffect/MainActivity.java
+++ app/src/main/java/com/zhx/videoeffect/MainActivity.java
@@ -2,13 +2,26 @@ package com.zhx.videoeffect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.os.Environment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "GLViewMediaActivity";
+    private GLSurfaceView glView;
+    public static final String videoPath = "/sdcard/DCIM/Camera/VID_20200617_193041.mp4";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        glView = findViewById(R.id.surface_view);
+        glView.setEGLContextClientVersion(2);
+        GLVideoRenderer glVideoRenderer = new GLVideoRenderer(this, videoPath);//创建renderer
+        glView.setRenderer(glVideoRenderer);//设置renderer
+
     }
 }
